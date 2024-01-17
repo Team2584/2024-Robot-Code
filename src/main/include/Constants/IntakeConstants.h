bb02de7712cbc0b835782c650aef2392b64e74f3
EricLindCS <107385095+EricLindCS@users.noreply.github.com>
2024-01-17 07:44:33 -0800

    Changed Wrist Constants

--- src/main/include/Constants/IntakeConstants.h
+++ src/main/include/Constants/IntakeConstants.h
@@ -1,21 +1,21 @@
-// Ports for different motors and sensors (Spark MAX CAN ID - Check using REV firmware utility)
-#define INTAKE_MOTOR_PORT 7 //????
+// Ports for different motors and sensors (Spark MAX CAN ID - Check/Set using REV firmware utility)
+#define INTAKE_MOTOR_PORT 7
 #define WRIST_MOTOR_PORT 21
 
 //Intake motor speeds, percent
-#define INTAKE_SPEED_IN 1
-#define INTAKE_SPEED_OUT 1
+#define INTAKE_SPEED_IN 1.0
+#define INTAKE_SPEED_OUT 1.0
 
 //Wrist absolute Encoder position values, 0-1 rotatons
-#define WRIST_HIGH 0.775
-#define WRIST_LOW 0.995
+#define WRIST_HIGH 0.733
+#define WRIST_LOW 0.999
 
-#define WRIST_SPEED_LOW_THRESHHOLD -0.2
+#define WRIST_SPEED_LOW_THRESHHOLD -2.0
 
 //Wrist PID Constants
 #define WRISTFF 0
-#define WRISTKP 0.4
+#define WRISTKP 2.0
 #define WRISTKI 0
 #define WRISTKIMAX 0.1
 #define ALLOWABLE_ERROR_WRIST 0.05  
-#define WRISTMAX_SPEED 0.5
+#define WRISTMAX_SPEED 0.7
