43c3c6615bedc9b66c0511dd230186abe53642bb
DRKDesigns <75279720+DRKDesigns@users.noreply.github.com>
2024-02-10 11:05:44 -0800

    Converted maps to enums
    Makes setting a LED strip effect much easier

--- src/main/cpp/LEDs.cpp
+++ src/main/cpp/LEDs.cpp
@@ -6,23 +6,15 @@
 LEDLights::LEDLights(frc::PWMSparkMax *lights_)
 {
   lights = lights_;
-  lightEffects["fire"] = -0.59;
-  lightEffects["red"] = 0.61;
-  lightEffects["orange"] = 0.65;
-  lightEffects["yellow"] = 0.69;
-  lightEffects["lime"] = 0.73;
-  lightEffects["green"] = 0.77;
-  lightEffects["blue"] = 0.81;
-  lightEffects["purple"] = 0.91;
-  SetLED("fire");
+  SetLED();
 }
 
 void LEDLights::SetLED()
 {
-  SetLED("fire");
+  SetLED(fire);
 }
 
-void LEDLights::SetLED(std::string color)
+void LEDLights::SetLED(LEDLights::lightEffect color)
 {
-  lights->Set(lightEffects[color]);
+  lights->Set(lightEffectIDs[color]);
 }
--- src/main/include/LEDs.h
+++ src/main/include/LEDs.h
@@ -1,15 +1,17 @@
 #include "Robot.h"
 #include "constants/LEDConstants.h"
 #include <frc/motorcontrol/PWMSparkMax.h>
+#include <iostream>
 
 class LEDLights{
     public:
-        std::map<std::string, float> lightEffects;
+        enum lightEffect{fire, red, orange, yellow, lime, green, blue, purple};
+        float lightEffectIDs[8] = {-0.59, 0.61, 0.65, 0.69, 0.73, 0.77, 0.81, 0.91};
         frc::PWMSparkMax* lights;
 
-        float currentLightEffect = -0.59;
+        float currentLightEffect = lightEffectIDs[lightEffect::fire];
 
-        void SetLED(std::string color);
+        void SetLED(lightEffect color);
         void SetLED();
 
         LEDLights(frc::PWMSparkMax *lights_);
