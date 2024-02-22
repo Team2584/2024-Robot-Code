1c7e5e7a01c86edd482788043e03467289a504b9
drago314 <avrickaltmann@yahoo.com>
2024-02-21 19:36:39 -0800

    Change PID constants such that they can be edited through smartdashbaord

--- src/main/include/Constants/FlywheelConstants.h
+++ src/main/include/Constants/FlywheelConstants.h
@@ -35,12 +35,12 @@ inline constexpr auto KG = 0.1625_V;
 inline constexpr auto KV = 0.4_V * 1_s / 1_rad; //0.88
 inline constexpr auto KP = 15;
 inline constexpr auto KI = 0;
-inline constexpr auto KI_MAX = 0; // In percent power
+inline constexpr auto KI_MAX = 0; 
 inline constexpr auto KD = 0;
-inline constexpr auto POS_TOLERANCE = 0.05; // In Degrees
-inline constexpr auto VELOCITY_TOLERANCE = 5; // In percent power
-inline constexpr auto MIN_SPEED = 0;// In percent power
-inline constexpr auto MAX_SPEED = 10; //0.3 // In percent power
+inline constexpr auto POS_TOLERANCE = 0.03;
+inline constexpr auto VELOCITY_TOLERANCE = 5; 
+inline constexpr auto MIN_SPEED = 0;
+inline constexpr auto MAX_SPEED = 10; 
 }
 
 /* Autonomous Constants */
