f8fc69e8c0e495ca3e712bbaf0be50ee38f7dcdc
Eric Lind <107385095+EricLindCS@users.noreply.github.com>
2024-03-17 06:00:55 -0700

    Elevator constants with new pulley

--- src/main/include/Constants/ElevatorConstants.h
+++ src/main/include/Constants/ElevatorConstants.h
@@ -10,11 +10,11 @@ namespace ElevatorConstants {
 inline units::meters_per_second_t kMaxVelocity = 1_mps;
 inline units::meters_per_second_squared_t kMaxAcceleration = 0.7_mps_sq;
 inline units::volt_t maxVoltsDown = 2_V;
-inline double m_kP = 36.0;
+inline double m_kP = 50.0; //36
 inline double m_kI = 0.05;
 inline double m_kD = 0.0;
 inline units::volt_t m_kS = 0_V;
-inline units::volt_t m_kG = 0.3_V;
+inline units::volt_t m_kG = 0.3_V; //BEFORE PULLEY CHANGE
 inline auto m_kV = 1.3_V / 1_mps;
 inline auto ALLOWABLE_ERROR_POS = 0.04_m;
 
@@ -24,8 +24,11 @@ inline auto ELEV_INTAKE = 0;
 inline auto ELEV_OUTTAKE = 0.15;
 inline auto ELEV_TRAP = 0.70;
 
+inline auto ELEV_SOURCE = 0.60; //fake value
+
+
 inline constexpr double gearRatioValue1 = 1.0/4.0;
-inline constexpr double diameterValue1 = 0.0762; //meters
+inline constexpr double diameterValue1 = 0.0762 / 2.0; //meters
 inline constexpr double ELEV_CONVERSION_FACTOR = gearRatioValue1 * (3.14159365 * diameterValue1);
 
 namespace AmpMech{
